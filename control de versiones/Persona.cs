06eec95087ee47faac5b45bf3ed79cf9a124d390
Jonatan Tel Ramos <jonatan.tel.ramos@cieep.com>
2021-01-19 11:46:08 +0100

    clase persona creada

---
+++ control de versiones/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace control_de_versiones
+{
+    class Persona
+    {
+    }
+}
