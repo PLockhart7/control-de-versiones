fe8a5a02922cc42c295d3c3fcd5e586c3d913a1d
Jonatan Tel Ramos <jonatan.tel.ramos@cieep.com>
2021-01-19 11:10:28 +0100

    primera faase completada

--- control de versiones/Form1.cs
+++ control de versiones/Form1.cs
@@ -16,5 +16,10 @@ namespace control_de_versiones
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hiiiii");
+        }
     }
 }
